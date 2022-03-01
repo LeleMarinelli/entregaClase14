ea21f57944888a22d609835ad15f489aec61d129
Lele Marinelli <leancymarinelli@gmail.com>
2022-02-28 23:56:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+
+const { Server: HttpServer } = require("http");
+const { Server: Socket } = require("socket.io");
+
+const ContenedorMemoria = require("./contenedores/ContenedorMemoria.js");
+const ContenedorArchivo = require("./contenedores/ContenedorArchivo.js");
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new Socket(httpServer);
+
+const productosApi = new ContenedorMemoria();
+const mensajesApi = new ContenedorArchivo("mensajes.json");
+
+// middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Socket io
+
+io.on("connection", async (socket) => {
+  console.log("Nuevo cliente conectado!");
+
+  //Productos///////////////////
+
+  socket.emit("productos", productosApi.listarAll());
+
+  socket.on("update", (producto) => {
+    productosApi.guardar(producto);
+    io.sockets.emit("productos", productosApi.listarAll());
+  });
+
+  // Mensajes///////////////////
+  socket.emit("mensajes", await mensajesApi.listarAll());
+
+  socket.on("nuevoMensaje", async (mensaje) => {
+    mensaje.fyh = new Date().toLocaleString();
+    await mensajesApi.guardar(mensaje);
+    io.sockets.emit("mensajes", await mensajesApi.listarAll());
+  });
+});
+
+const PORT = 8080;
+const connectedServer = httpServer.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
+connectedServer.on("error", (error) => console.log("Error: ", error));
